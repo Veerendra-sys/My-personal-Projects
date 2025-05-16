e0551f5172a0230f703ca7a702bc6b302f306390
Amaravathi Veerendra nath <73882617+Veerendra-sys@users.noreply.github.com>
2025-05-16 15:47:32 +0530

    Add files via upload

---
+++ Projects/Computer Vision AIVirtual Mouse Project/main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+    if result.multi_hand_landmarks:
+        for hand in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, hand, mp_hands.HAND_CONNECTIONS)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
